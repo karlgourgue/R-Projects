15d89d20205f015192492f32b49637b7cf629db3
Karl Gourgue <karlgourgue@gmail.com>
2017-03-02 06:31:49 -0500

    Added comments

--- Baby Names Analysis/Baby Names Script.R
+++ Baby Names Analysis/Baby Names Script.R
@@ -1,9 +1,8 @@
 # Part 1, Loading Files Into R
-setwd("~/Downloads/Names")
-
 a = seq(1880,2014)
 babynames = NULL
-for (i in a) {
+#Files are in a folder with csv files, each labeled "yob1880.txt" -> "yob2014.txt"
+for (i in a) { #a loop that loads each file into a df "babynams"
   temp = read.csv(paste("yob", toString(i), ".txt", sep=''), header=FALSE)
   b = c(rep(i, nrow(temp)))
   d = cbind(temp,b)
@@ -12,26 +11,28 @@ for (i in a) {
 
 names(babynames) = c("Name", "Gender", "Count", "Year")
 
-#Part 2, The Popularity of "Karl"
+#Part 2, The Popularity of "Karl" (my name) Over TIME
 myname = babynames[babynames$Name == "Karl" & babynames$Gender == "M",]
 plot(myname$Year,myname$Count,xlab="Year",ylab="Count",main="Popularity of Karl over time")
 
 
-#Part 3, Unique Girl Names Over Time
+#Part 3, Plotting Number of Unique Girl Names Over Time
 onlygirls = babynames[babynames$Gender == "F",]
 onlygirlsf = data.frame(table(onlygirls$Year))
 names(onlygirlsf) = c("Year","Count")
 plot(onlygirlsf$Year,onlygirlsf$Count,xlab="Year",ylab="Number of Unique Names",main="Growth in Unique Names")
 
-#Part 4, Most Popular Name / Gender + Year
+#Part 4, Finding Most Popular Name / Gender + Year
 toptennames <- function (year,gender) {
   ttn = babynames[babynames$Year == year & babynames$Gender == gender,]
   ttn[order("Count",decreasing = TRUE)]
   head(ttn, n=10)
-}
+} #function takes year,gender as input and outputs top 10 names
 
 
-#Part 5, Most Gender Neutral Names
+#Part 5, Fingin Most Gender Neutral Names
+#Accomplished by ranking frequency of male + female names, then putting those together to see 
+#which names rank highest across both
 male = babynames[babynames$Year == 2014 & babynames$Gender == "M",]
 male = cbind(male, rank(-male$Count))
 male = subset(male, select=c("Name", "rank(-male$Count)"))
@@ -47,22 +48,3 @@ names(neutral)[names(neutral)=="rank(-female$Count)"] <- "frank"
 maxrank = pmax(neutral$mrank,neutral$frank)
 neutral = cbind(neutral,maxrank)
 head(neutral[order(maxrank),])
-
-
-#Part 6, Expected Age of Class
-age = read.csv("classnames.csv")
-pvallist = NULL
-years = c(1880:2014)
-
-agep = function (name) {
-  for (num in years) {
-    countsum = sum(babynames[babynames$Name == name & babynames$Year == num,]$Count)
-    total = sum(babynames[babynames$Year == num,]$Count)
-    prob = countsum/total
-    quicknamelist = c(name)
-    pvallist = lapply(quicknamelist,agep) 
-  }
-  return (head(pvallist))
-}
-
-#This doesn't work, I would probably loop everything into a list and plot it against years.
